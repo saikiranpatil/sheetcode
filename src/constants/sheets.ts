af5b54146550f886144c21180c690774dfa35dfa
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2025-09-01 23:51:12 +0530

    added 2 sheets, correct quick tour image

--- src/constants/sheets.ts
+++ src/constants/sheets.ts
@@ -1,8 +1,10 @@
-import neetcode75 from "@/assets/sheets/Neetcode75.json";
+import neetcode150 from "@/assets/sheets/Neetcode150.json";
+import blind75 from "@/assets/sheets/Blind75.json";
 import { Sheet } from "../types";
 
 export const sheetsData: Omit<Sheet, 'id'>[] = [
-    neetcode75
+    neetcode150,
+    blind75
 ];
 
 export const sheetIdParamsTitle = "sheetcodeSheetId";
