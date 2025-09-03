e09a1c94c24614e216a3f46029d9fa69e6e26667
Saikiran Patil <84263946+saikiranpatil@users.noreply.github.com>
2025-09-03 13:26:03 +0530

    minor changes, remove unneeded permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -13,20 +13,20 @@ export default defineConfig({
     name: "Sheetcode Extension",
     version: "0.0.1",
     description: "A simple Chrome extension to keep track of coding problems solved on different platforms.",
-    permissions: ["activeTab", "tabs", "storage"],
+    permissions: ["activeTab", "tabs"],
     icons: {
-      16: "icon/16X16.png",
-      32: "icon/32X32.png",
-      192: "icon/192X192.png",
-      180: "icon/180X180.png"
+      16: "icon/16x16.png",
+      32: "icon/32x32.png",
+      192: "icon/192x192.png",
+      180: "icon/180x180.png"
     },
     action: {
       default_title: "Sheetcode",
       default_icon: {
-        16: "icon/16X16.png",
-        32: "icon/32X32.png",
-        192: "icon/192X192.png",
-        180: "icon/180X180.png"
+        16: "icon/16x16.png",
+        32: "icon/32x32.png",
+        192: "icon/192x192.png",
+        180: "icon/180x180.png"
       }
     }
   },
